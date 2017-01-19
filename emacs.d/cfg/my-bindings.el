d128cf3718e6a1ff1df6f588b76d58adfac71cc7
Jack Zhou <jack@zzh.ca>
2017-01-19 15:59:42 -0500

    Add vim window-moving bindings to Emacs

--- emacs.d/cfg/my-bindings.el
+++ emacs.d/cfg/my-bindings.el
@@ -6,6 +6,11 @@
 
 ;; Key bindings
 (global-set-key (kbd "<escape>") 'keyboard-quit)
+(global-unset-key (kbd "C-w"))
+(global-set-key (kbd "C-w h") 'windmove-left)
+(global-set-key (kbd "C-w j") 'windmove-down)
+(global-set-key (kbd "C-w k") 'windmove-up)
+(global-set-key (kbd "C-w l") 'windmove-right)
 
 (provide 'my-bindings)
 ;;; my-bindings.el ends here
