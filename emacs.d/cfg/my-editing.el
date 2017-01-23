0f3d7fe3c36970ce41cda32fc67e4a042bebb8b5
Jack Zhou <jack@zzh.ca>
2017-01-23 16:40:56 -0500

    Move Emacs temp files to temp directory

--- emacs.d/cfg/my-editing.el
+++ emacs.d/cfg/my-editing.el
@@ -15,6 +15,12 @@
 (electric-pair-mode)
 (global-visual-line-mode)
 
+;; Move backups to temp directory
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+
 ;; Prevent killing to clipboard
 (setq select-enable-clipboard nil)
 (setq select-enable-primary t)
