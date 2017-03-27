184fa583200061a6438dfb0f40f5910a6e66025f
Jack Zhou <jack@zzh.ca>
2017-03-27 19:58:43 -0400

    Fix escape key in Emacs

--- emacs.d/cfg/my-bindings.el
+++ emacs.d/cfg/my-bindings.el
@@ -5,7 +5,6 @@
 ;;; Code:
 
 ;; General key bindings
-(global-set-key (kbd "<escape>") 'keyboard-quit)
 (global-unset-key (kbd "C-w"))
 (global-set-key (kbd "C-w h") 'windmove-left)
 (global-set-key (kbd "C-w j") 'windmove-down)
@@ -15,6 +14,26 @@
 ;; Org-mode key bindings
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
+
+;; Fix ESC key in Evil mode
+;;; esc quits
+(defun minibuffer-keyboard-quit ()
+  "Abort recursive edit.
+In Delete Selection mode, if the mark is active, just deactivate it;
+then it takes a second \\[keyboard-quit] to abort the minibuffer."
+  (interactive)
+  (if (and delete-selection-mode transient-mark-mode mark-active)
+      (setq deactivate-mark  t)
+    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+    (abort-recursive-edit)))
+(define-key evil-normal-state-map [escape] 'keyboard-quit)
+(define-key evil-visual-state-map [escape] 'keyboard-quit)
+(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+(global-set-key [escape] 'evil-exit-emacs-state)
 
 (provide 'my-bindings)
 ;;; my-bindings.el ends here
