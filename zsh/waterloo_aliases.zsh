2dd5ff7b56ad3dfaf9911f44084babc9d3325641
Jack Zhou <jack@zzh.ca>
2018-05-05 23:29:31 -0400

    all: Update UWaterloo aliases

--- zsh/waterloo_aliases.zsh
+++ zsh/waterloo_aliases.zsh
@@ -1,11 +1,10 @@
 # Classes
-alias cs343="cd ~/Waterloo/CS\ 343"
-alias cs348="cd ~/Waterloo/CS\ 348"
-alias cs488="cd ~/Waterloo/CS\ 488"
-alias phil226="cd ~/Waterloo/PHIL\ 226"
-alias se380="cd ~/Waterloo/SE\ 380"
-alias se390="cd ~/Waterloo/SE\ 390"
-alias se464="cd ~/Waterloo/SE\ 464"
+alias cs452="cd ~/Waterloo/CS\ 452"
+alias cs466="cd ~/Waterloo/CS\ 466"
+alias ece458="cd ~/Waterloo/ECE\ 458"
+alias se463="cd ~/Waterloo/SE\ 463"
+alias se490="cd ~/Waterloo/SE\ 490"
+alias wkrt400="cd ~/Waterloo/WKRT\ 400"
 
 # Other
 alias octave="octave --no-gui"
