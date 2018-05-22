88ded317d7f8d9fea3cc242315aba1f72f97d0c7
Jack Zhou <jack@zzh.ca>
2018-05-22 01:55:26 -0400

    emacs: Improve consistency of configuration files

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -5,37 +5,31 @@
 ;;; Code:
 
 ;; Load all other settings
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'org)
 (setq vc-follow-symlinks t)
 (org-babel-load-file
  (expand-file-name (concat user-emacs-directory "config.org")))
 
-;; Built-in options
-(setq package-enable-at-startup nil)
-(package-initialize)
+(provide 'init)
+;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
- '(inhibit-startup-screen t)
- '(initial-buffer-choice t)
- '(org-agenda-files (quote ("~/Drive/planner.org")))
  '(package-selected-packages
    (quote
-    (which-key cmake-ide color-theme-solarized emacs-color-theme-solarized irony company-irony eyebrowse markdown-preview-eww toml-mode emmet-mode highlight-chars ido-ubiquitous markdown-preview-mode markdown-mode: markdown-mode ggtags evil-leader use-package solarized-theme isend-mode helm-projectile god-mode git-gutter flycheck-haskell fill-column-indicator exec-path-from-shell evil-tabs evil-surround evil-magit evil-commentary editorconfig dtrt-indent delight column-marker)))
- '(safe-local-variable-values (quote (insert-tabs-mode)))
- '(shell-escape-mode "-shell-escape"))
+    (ws-butler which-key solarized-theme toml-mode racer flycheck-rust markdown-preview-eww cargo rust-mode projectile markdown-mode latex-preview-pane isend-mode smex ido-vertical-mode ido-completing-read+ flx-ido git-gutter ggtags flycheck-irony flycheck-ghcmod flycheck exec-path-from-shell eyebrowse evil-surround evil-matchit evil-magit evil-commentary evil-leader evil emmet-mode editorconfig dtrt-indent diminish delight company-jedi jedi-core company-web company-tern company-math company-irony company-ghc company-flx company cmake-ide use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'dired-find-alternate-file 'disabled nil)
-
-(provide 'init)
-;;; init.el ends here
