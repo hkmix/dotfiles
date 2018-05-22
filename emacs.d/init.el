839cb97b494619c2b516778e41011f2488f0b654
Jack Zhou <jack@zzh.ca>
2018-05-22 02:53:59 -0400

    emacs: Move end of init file block to end of file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -17,8 +17,6 @@
 (org-babel-load-file
  (expand-file-name (concat user-emacs-directory "config.org")))
 
-(provide 'init)
-;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -33,3 +31,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(provide 'init)
+;;; init.el ends here
