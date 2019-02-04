ffe81d8c1135bbab7123298d3553add36848ac5a
Jack Zhou <jack@zzh.ca>
2019-02-04 15:25:34 -0500

    zsh: Add aliases to update school packages

--- base/.zsh/waterloo_aliases.zsh
+++ base/.zsh/waterloo_aliases.zsh
@@ -7,3 +7,11 @@ alias ece457b="cd ~/Waterloo/ECE\ 457B"
 function wcd() {
     cd "$HOME/Waterloo Projects/$@"
 }
+
+function wup() {
+    classes=("cs451" "ece459")
+    for class in "${classes[@]}"; do
+        printf "\e[97;1m==> Updating %s...\e[0m\n" "$class"
+        (cd "$HOME/Waterloo Projects/$class" && git pull)
+    done
+}
