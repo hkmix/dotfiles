af1912ebe40c99bd1b216d248c46be912585c80f
Jack Zhou <jack@zzh.ca>
2022-12-26 15:33:03 -0500

    doc: Avoid re-adding xkb entries if present

--- base/.doc/apply_xkb.sh
+++ base/.doc/apply_xkb.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-sudo cat >>/usr/share/X11/xkb/symbols/us <<EOF
+_target_file=/usr/share/X11/xkb/symbols/us
 
+grep -o 'BEGIN XKB BLOCK' $_target_file >/dev/null && echo "Nothing to be done." || sudo cat >>$_target_file <<EOF
+
+// BEGIN XKB BLOCK.
 partial modifier_keys
 xkb_symbols "hhkb" {
-	include "us(basic)"
+    include "us(basic)"
+    key <END> { [ Insert ] };
     key <BKSP> { [ backslash, bar ] };
     key <BKSL> { [ BackSpace, BackSpace ] };
 
@@ -15,9 +19,11 @@ xkb_symbols "hhkb" {
 
 partial modifier_keys
 xkb_symbols "hhkb_nobs" {
-	include "us(basic)"
+    include "us(basic)"
+    key <END> { [ Insert ] };
     key <LALT> { type[Group1] = "ONE_LEVEL",
                  symbols[Group1] = [ Super_L ] };
     key <LWIN> { [ Alt_L, Meta_L ] };
 };
+// END XKB BLOCK.
 EOF
